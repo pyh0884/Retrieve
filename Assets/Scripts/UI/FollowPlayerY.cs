abf754906ccae7bc70fd9aa715d18a6731a52299
pyh0884 <pyh2084@hotmail.com>
2019-09-10 20:57:27 +0800

    1

---
+++ Assets/Scripts/UI/FollowPlayerY.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerY : MonoBehaviour
+{
+    GameObject player;
+    public float smooth;
+    private Transform cam;
+    private Vector3 previousCamPos;
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    void Start()
+    {
+        previousCamPos = cam.position;
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//if(!player) player = GameObject.FindGameObjectWithTag("Player");
+        float parallax = (previousCamPos.y - cam.position.y)* -1;
+        float backgroundTargetPosY = transform.position.y + parallax;
+        Vector3 backgroundTargetPos = new Vector3(transform.position.x, backgroundTargetPosY, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, backgroundTargetPos, smooth * Time.deltaTime);
+        previousCamPos = cam.position;
+
+        //transform.position = new Vector3(transform.position.x, player.transform.position.y + 5, transform.position.z);
+
+        //transform.position = Vector3.Lerp(transform.position,new Vector3(transform.position.x,player.transform.position.y+5,transform.position.z), smooth * Time.deltaTime);
+    }
+}
--- Assets/Scripts/UI/ParallaxBKG.cs
+++ Assets/Scripts/UI/ParallaxBKG.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class ParallaxBKG : MonoBehaviour
 {
-    public Transform[] backgrounds;
-    private float[] parallaxScales;
+    private float parallaxScales;
     public float smoothing = 1f;
     private Transform cam;
     private Vector3 previousCamPos;
@@ -16,22 +15,16 @@ public class ParallaxBKG : MonoBehaviour
     void Start()
     {
         previousCamPos = cam.position;
-        parallaxScales = new float[backgrounds.Length];
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            parallaxScales[i] = backgrounds[i].position.z * -1;
-        }
+        parallaxScales= transform.position.z * -1;
     }
 
     void Update()
     {
-        for (int i = 0; i < backgrounds.Length; i++)
-        {
-            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
-            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
-            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX,backgrounds[i].position.y,backgrounds[i].position.z);
-            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position,backgroundTargetPos,smoothing*Time.deltaTime);
-            previousCamPos = cam.position;
-        }
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales;
+        float backgroundTargetPosX = transform.position.x + parallax;
+
+        Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, backgroundTargetPos, smoothing * Time.deltaTime);
+        previousCamPos = cam.position;
     }
 }
