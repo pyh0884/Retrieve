8328654ab94223f4b54efc183bc0fd70671a8f73
fakeBoomerang <zhaolei1@mail.ustc.edu.cn>
2019-09-12 21:04:39 +0800

    Shield和button

---
+++ Assets/Scripts/Enemy/Shield.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+	public GameObject buttonPrefab;
+	public List<Transform> spawnPos;
+	// Start is called before the first frame update
+	// Update is called once per frame
+	void Update()
+    {
+		int temp = 0;
+		for (int i = 0; i < spawnPos.Count; i++) temp += spawnPos[i].childCount;
+		if (temp==0) gameObject.SetActive(false);
+    }
+	private void OnEnable()
+	{
+		for (int i = 0; i < spawnPos.Count; i++) {
+			Instantiate(buttonPrefab, spawnPos[i],false);
+		}
+	}
+}
