0f6cd916db6f70781305b40c7ae8763894570848
fakeBoomerang <zhaolei1@mail.ustc.edu.cn>
2019-09-13 14:50:06 +0800

    传送门

--- Assets/Scripts/Items/CheckPointCheckPoint.cs
+++ Assets/Scripts/Items/CheckPointCheckPoint.cs
@@ -14,7 +14,7 @@ public class CheckPointCheckPoint : MonoBehaviour
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		if (collision.tag == "Player") {
+		if (collision.CompareTag("Player")) {
 			UpdateSaveData(transform.position,sceneIndex);
 		}
 	}
