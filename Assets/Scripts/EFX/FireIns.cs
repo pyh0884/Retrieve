50d23b9ca58351e4a39e201c2e0db6f3f7cf2886
pyh0884 <pyh2084@hotmail.com>
2019-10-02 01:35:28 +0800

    1

---
+++ Assets/Scripts/EFX/FireIns.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireIns : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+    public GameObject fire;
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+        while (i < numCollisionEvents)
+        {
+            if (other.layer==8)
+                Instantiate(fire, new Vector3(Mathf.RoundToInt(collisionEvents[i].intersection.x*2)/2f, collisionEvents[i].intersection.y), Quaternion.identity);
+            
+            i++;
+        }
+    }
+}
