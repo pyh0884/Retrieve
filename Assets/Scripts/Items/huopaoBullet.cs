e20b0af192c8486678bc6e9afc15c20d68637bc2
pyh0884 <pyh2084@hotmail.com>
2019-10-06 02:04:28 +0800

    1

---
+++ Assets/Scripts/Items/huopaoBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class huopaoBullet : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed=10;
+    public bool TowardsRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, 3);
+        rb.velocity = new Vector2(TowardsRight ? speed : -speed, 0);
+    }
+}
