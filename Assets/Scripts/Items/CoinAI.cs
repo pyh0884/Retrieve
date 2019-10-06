b4a0bb9a5fa0c940bbdcca70743d08b0aba248a6
pyh0884 <pyh2084@hotmail.com>
2019-10-07 06:56:29 +0800

    1

---
+++ Assets/Scripts/Items/CoinAI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAI : MonoBehaviour
+{
+    GameManager gm;
+    public int MoneyValue = 1;
+    private bool Enter;
+    public float speed=3;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+        anim = GetComponent<Animator>();
+    }
+    void MoveToWards()
+    {
+        transform.parent.transform.position = Vector3.Lerp(transform.parent.transform.position, GameObject.Find("PlayerTransform").transform.position, Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Enter = true;
+            gm.GetMoney(MoneyValue);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Enter)
+        {
+            MoveToWards();
+
+            if (Mathf.Abs(transform.parent.transform.position.x-GameObject.Find("PlayerTransform").transform.position.x)<1)
+            {
+                GetComponentInParent<Rigidbody2D>().velocity = Vector3.zero;
+                anim.SetTrigger("Hit");
+                Destroy(gameObject.transform.parent.gameObject, 1);
+            }
+        }
+    }
+}
