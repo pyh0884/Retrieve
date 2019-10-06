5b6113eb8ca3dd1ce9bf4852482ffce49cc80975
pyh0884 <pyh2084@hotmail.com>
2019-10-06 14:12:51 +0800

    1

--- Assets/Scripts/Items/huopao.cs
+++ Assets/Scripts/Items/huopao.cs
@@ -8,11 +8,30 @@ public class huopao : MonoBehaviour
     public GameObject rightBullet;
     public Transform trans;
     public bool TowardsRight;
-public void shoot()
+    public float CDTime;
+    float timer=100;
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void shoot()
     {if (TowardsRight)
             Instantiate(rightBullet, trans.position, Quaternion.identity);
     else
             Instantiate(leftBullet, trans.position, Quaternion.identity);
 
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (timer>CDTime&&collision.tag == "Player")
+        {
+            anim.SetTrigger("Attack");
+            timer = 0;
+        }
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
     }
 }
