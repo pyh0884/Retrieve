894e05026d4e643edeee65b50a0e49265a051bf3
fakeBoomerang <zhaolei1@mail.ustc.edu.cn>
2019-10-06 15:37:50 +0800

    鱼叉

--- Assets/Scripts/Enemy/FishPeddleVice.cs
+++ Assets/Scripts/Enemy/FishPeddleVice.cs
@@ -17,51 +17,26 @@ public class FishPeddleVice : MonoBehaviour
     void Start()
     {
 		anim = GetComponent<Animator>();
-		main = transform.parent.gameObject;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-		if (main == null) Destroy(gameObject);
     }
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("PlayerAttack"))
 		{
-			StartCoroutine(GotHit(transform));
 			anim.SetTrigger("Die");
 		}
-        if (collision.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
-                Destroy(gameObject);
-                if (deadBody)
-                {
-                    Instantiate(deadBody, transform.position, Quaternion.identity);
-                }
-            
-        }
-        if ((collision.gameObject.layer == 8 || collision.gameObject.layer == 12))
-        {
-            if(deadBody)
-            Instantiate(deadBody, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
-    }
-
-	IEnumerator GotHit(Transform curr)
-	{
-		Vector3 target;
-		int frameCount = 0;
-		target = curr.position + curr.right * hitBackDist;
-		//if (right) target = transform.position + new Vector3(hitBackDist, 0, 0);
-		//else target = transform.position + new Vector3(-hitBackDist, 0, 0);
-		while (frameCount < maxBackFrame)
+            if (deadBody)
+            {
+				Instantiate(deadBody, transform.position, Quaternion.identity);
+            }
+			anim.SetTrigger("Die");
+		}
+		if ((collision.gameObject.layer == 8 || collision.gameObject.layer == 12))
 		{
-			curr.position = Vector3.MoveTowards(curr.position, target, hitBackSpeed * Time.deltaTime);
-			yield return null;
-			frameCount++;
+			if (deadBody)Instantiate(deadBody, transform.position, Quaternion.identity);
+			anim.SetTrigger("Die");
 		}
 	}
 }
