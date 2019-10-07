89f89ce7c04d5bac2e5532ccac0bb371e79b804b
pyh0884 <pyh2084@hotmail.com>
2019-10-07 12:51:32 +0800

    1

--- Assets/Scripts/Items/CoinsGenerator.cs
+++ Assets/Scripts/Items/CoinsGenerator.cs
@@ -13,14 +13,32 @@ public class CoinsGenerator : MonoBehaviour
         while (value > 0)
         {
             var ins1 = Instantiate(coins[Mathf.FloorToInt(Random.Range(0,coins.Length-0.5f))],transform.position,Quaternion.identity);
-            ins1.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(0, 10) > 5 ? 3 : -3, 7);
+            ins1.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(0, 10) > 5 ? Random.Range(2,4) : -Random.Range(2, 4), Random.Range(5, 7));
             value -= ins1.GetComponentInChildren<CoinAI>().MoneyValue;
 
         }
         }
-    // Update is called once per frame
-    void Update()
+    public void GenCoinsInChest()
     {
-        
+        while (value > 0)
+        {
+            var ins1 = Instantiate(coins[Mathf.FloorToInt(Random.Range(0, coins.Length - 0.5f))], transform.position, Quaternion.identity);
+            ins1.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(0, 10) > 5 ? Random.Range(1, 7) : -Random.Range(1, 7), Random.Range(3, 10));
+            value -= ins1.GetComponentInChildren<CoinAI>().MoneyValue;
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isChest && collision.tag == "PlayerAttack")
+        {
+
+               GetComponent<Animator>().SetTrigger("Open");
+
+        }
+    }
+    public void des()
+    {
+        Destroy(gameObject);
     }
 }
