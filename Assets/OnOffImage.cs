6fe7cfe8d3095e3c6687b6471256f5a535e2857d
pyh0884 <pyh2084@hotmail.com>
2019-10-09 17:29:27 +0800

    1

---
+++ Assets/OnOffImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class OnOffImage : MonoBehaviour
+{
+    public Sprite[] spr;
+    public Image img; 
+    public bool Ischecked;
+    // Start is called before the first frame update
+    public void ische()
+    {
+        if (Ischecked)
+            Ischecked = false;
+        else
+            Ischecked = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ischecked)
+        {
+            img.sprite = spr[0];
+        }
+        else
+        {
+            img.sprite = spr[1];
+        }
+    }
+}
