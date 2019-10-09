4bda05d1a9bb5d82d8a99dd54e26d448d2d26a32
fakeBoomerang <zhaolei1@mail.ustc.edu.cn>
2019-10-09 21:15:59 +0800

    emmmm

---
+++ Assets/Scripts/Exinrende.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exinrende : MonoBehaviour
+{
+	bool start;
+	bool input;
+	public int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (start) {
+			switch (num) {
+				case 0: input=Input.GetButtonDown("Jump");break;
+				case 1: input = Input.GetButtonDown("Fire2");break;
+				case 2: input = Input.GetButtonDown("Fire1")||Input.GetAxisRaw("Fire1")>0.5f;break;
+			}
+			if (input)
+			{
+				Time.timeScale = 1;
+				Destroy(this);
+			}
+		}
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("Player"))
+		{
+			Time.timeScale = 0;
+			start = true;
+		}
+	}
+}
